9ec787455356f4825baa96fa39fd14037a047c45
JangGyungSeok <juskus0310@gmail.com>
2020-03-16 16:54:52 +0900

    flask 기본형태, json형태로 반환하는 방법 등!

---
+++ test.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, escape, request, render_template
+
+
+app = Flask(__name__)
+
+
+# app.route 를 사용해 매핑해준다.
+# render_template -> 사용해 templates의 html 파일을 불러오겠다는 뜻
+
+@app.route('/main')
+def hello():
+    name = request.args.get("name", "World")
+#    return f'Hello, {escape(name)}!'
+    return render_template('main.html')
+
+@app.route('/testapi')
+def test():
+    return "안녕하세요?";
+
+@app.route('/testjson')
+def json():
+    # flask 에서 기본적으로 제공하는 jsonify함수를 통해 값을 json형태로 전환할 수 있다.
+    return jsonify(name='JKS')
+
+if __name__ == '__main__':
+    app.run(debug=True)
