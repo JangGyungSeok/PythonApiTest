27d6d011965e8bc8d4501626e497c87a19dd9614
JKS <juskus0310@naver.com>
2020-03-24 11:08:51 +0900

    Spring Boot와 연동할 기본적인 API서버형태 (대부분 미구현)

--- app.py
+++ app.py
@@ -13,13 +13,16 @@ def hello():
 #    return f'Hello, {escape(name)}!'
     return render_template('main.html')
 
-@app.route('/testapi')
+@app.route('/testapi',methods=["POST","GET"])
 def test():
-    return "안녕하세요?";
+    print("실행!!")
+    
+    return "보내준 메시지"
 
 @app.route('/testjson')
 def json():
     # flask 에서 기본적으로 제공하는 jsonify함수를 통해 값을 json형태로 전환할 수 있다.
+    print(jsonify(name='JKS'))
     return jsonify(name='JKS')
 
 if __name__ == '__main__':
